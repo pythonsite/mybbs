b165f5264b4f268eeae4a83d67e047f4d9f708ea
zhaofan <hjzhaofan@163.com>
2019-01-18 07:45:20 +0800

    登录页面

--- routers/router.go
+++ routers/router.go
@@ -8,4 +8,5 @@ import (
 func init() {
 	// beego.Router("/", &controllers.MainController{})
 	beego.Router("/", &controllers.IndexController{},"GET:Index")
+	beego.Router("/about", &controllers.IndexController{}, "GET:About")
 }
